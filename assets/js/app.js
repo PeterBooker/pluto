6a0e7cfbe19e34c68109b999310d34e20923a997
Peter Booker <peter.booker@coastdigital.co.uk>
2019-09-25 13:29:49 +0100

    Add basics of service worker

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Main App Script
+ */
+
+import serviceWorker from './serviceWorker'
+
+serviceWorker.unregister()
+//serviceWorker.register()
---
+++ assets/js/serviceWorker.js
@@ -0,0 +1,75 @@
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
+export function register(config) {
+  console.log('Register Service Worker')
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    // The URL constructor is available in all browsers that support SW.
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
+
+    window.addEventListener('load', () => {
+      const swUrl = `/pluto/service-worker.js`
+      // Register service worker
+      registerValidSW(swUrl, config)
+    })
+  }
+}
+
+function registerValidSW(swUrl, config) {
+  navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if (installingWorker == null) {
+            return;
+          }
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                // At this point, the updated precached content has been fetched,
+                // but the previous service worker will still serve the older
+                // content until all client tabs are closed.
+                console.log(
+                  'New content is available and will be used when all ' +
+                    'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                );
+  
+                // Execute callback
+                if (config && config.onUpdate) {
+                  config.onUpdate(registration);
+                }
+              } else {
+                // At this point, everything has been precached.
+                // It's the perfect time to display a
+                // "Content is cached for offline use." message.
+                console.log('Content is cached for offline use.');
+  
+                // Execute callback
+                if (config && config.onSuccess) {
+                  config.onSuccess(registration);
+                }
+              }
+            }
+          };
+        };
+  })
+  .catch(error => {
+    console.error('Error during service worker registration:', error);
+  });
+}
+
+export function unregister() {
+  console.log('Unregister Service Worker')
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+    });
+  }
+}
